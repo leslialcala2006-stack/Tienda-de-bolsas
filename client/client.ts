f095b5355a95105b86e2417c907a21c44cac44d0
leslialcala2006-stack <leslialcala2006@gmail.com>
2026-03-09 10:05:16 -0600

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -1,3 +1,5 @@
+import * as web3 from "@solana/web3.js";
 console.log("Dirección del cliente:", pg.wallet.publicKey.toString());
-const balance = await pg.connection.getBalance(pg.wallet.publicKey);
-console.log(`Saldo disponible: ${balance / web3.LAMPORTS_PER_SOL} SOL`);
+const balanceLamports = await pg.connection.getBalance(pg.wallet.publicKey);
+const balanceSol = balanceLamports / web3.LAMPORTS_PER_SOL;
+console.log(`Saldo disponible: ${balanceSol} SOL`);
